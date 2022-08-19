88a5c11dcfbb2410274a9cbb41c64f37c222d469
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-08-19 14:17:58 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Jane',
+      gender: 'Female' },
+    {
+      name: 'Richie',
+      gender: 'Male' } ,
+    {
+      name: 'Jeniffer',
+      gender: 'Female' },
+    {
+      name: 'Iva',
+      gender: 'Female' },
+    {
+      name: 'Vero',
+      hometown: 'Female' }
+  ];
+
+
+const findMatching = (drivers, value) => {
+     return drivers.filter((fn) => fn.toLowerCase() === value.toLowerCase());
+}
+
+const fuzzyMatch = (drivers, value) => {
+    return drivers.filter((fn) => fn[0] === value[0]      )
+}
+const matchName = function(drivers, value){
+    return drivers.filter(item => item.name === value);
+}  
